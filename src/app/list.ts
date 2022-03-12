1ed6bbaae61f9dc6d8e1a05ae0fcd41d8bf2ee32
christianpinheiro <93pinheiro@gmail.com>
2022-03-11 21:08:19 -0300

    3 files modified

--- src/app/list.ts
+++ src/app/list.ts
@@ -1,5 +1,6 @@
 export const listData = [
-  {id: 1, text: 'Texto 1'},
+  {text1: 'Recursos e aplicações da tecnologia escolhida', text2: 'Apresentar tecnologias ainda não estudadas ou aprofundar algum tema já visto definido pela coordenadoria do curso em cada unidade. Objetivos específicos. Estudar e aplicar tecnologias de uso corrente na região na qual o curso de insere ou de interesse para o desenvolvimento profissional dos estudantes ou desenvolver temas que aprofundem algum conhecimento já estudado.'},
   {id: 2, text: 'Texto 2'}, 
-  {id: 3, text: 'Texto 3'}
+  {id: 3, text: 'Texto 3'},
+  {id: 3, text: 'Texto 4'}
 ]
